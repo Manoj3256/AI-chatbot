cc0cdaa93a991e72e21bb5c23a97a1b9a7820330
Manoj3256 <3256mmanoj@gmail.com>
2026-07-30 19:55:51 +0530

    Simple frontend

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 class chat_define(BaseModel):
     message: str
 
